6c7933f6e8677c2ed70bb9ac10593e95b4477362
Henry Yeh <heyeh@icloud.com>
2022-12-06 18:14:39 +0800

    added console logs for debug

--- app.js
+++ app.js
@@ -15,7 +15,9 @@ App({
             method: "POST",
             data: { code: res.code },
             success(loginRes) {
+                console.log("LOGIN RES", loginRes)
                 page.globalData.user = loginRes.data.user;
+                console.log("SET USER TO GLOBALDATA", page.globalData.user)
                 page.globalData.header = loginRes.header['Authorization']
                 event.emit('tokenReady')
             },
